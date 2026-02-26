2d4fc4629f0c82bdeba880debef9db0ed77806e8
mentolbreak1 <98565793+mentolbreak1@users.noreply.github.com>
2026-02-26 18:33:19 +0300

    Update urls.py

--- news_project/news/urls.py
+++ news_project/news/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from . import views
 
+app_name = 'news'
+
+urlpatterns = [
+    path('', views.ArticleListView.as_view(), name='article_list'),
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
+]
